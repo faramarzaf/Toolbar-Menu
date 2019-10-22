cfa6a53e133fb873cff323c9bacbec35087c0626
FaramarzAf <faramarzafzali@gmail.com>
2019-10-22 11:49:49 +0330

    navigation drawer added

--- app/src/main/java/com/faramarz/tictacdev/toolbar/toolbar/ToolbarActivity.java
+++ app/src/main/java/com/faramarz/tictacdev/toolbar/toolbar/ToolbarActivity.java
@@ -4,19 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.view.ContextThemeWrapper;
 import androidx.appcompat.widget.PopupMenu;
 import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.faramarz.tictacdev.toolbar.R;
+import com.google.android.material.navigation.NavigationView;
 
 public class ToolbarActivity extends AppCompatActivity implements View.OnClickListener {
     Toolbar toolbar;
-    ImageButton btnCredit, btnMore;
+    ImageButton btnCredit, btnMore, btnDrawer;
+    DrawerLayout drawerLayout;
+    NavigationView navView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +34,32 @@ public class ToolbarActivity extends AppCompatActivity implements View.OnClickLi
         setSupportActionBar(toolbar);
         bind();
         clickEvents();
-    }
+
+
+        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                Fragment f = null;
+                int itemId = menuItem.getItemId();
+                if (itemId == R.id.creditItem) {
+                    f = new CreditFragment();
+                } else if (itemId == R.id.bankAccount) {
+                    f = new AccountFragment();
+                }
+                if (f != null) {
+                    FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                    transaction.replace(R.id.frame, f);
+                    transaction.commit();
+                    drawerLayout.closeDrawers();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+      }
+
+
 
     @Override
     public void onClick(View v) {
@@ -38,6 +72,14 @@ public class ToolbarActivity extends AppCompatActivity implements View.OnClickLi
             case R.id.btnMore:
                 onPopupButtonClick(btnMore);
                 break;
+
+            case R.id.btnDrawer:
+                if (!drawerLayout.isDrawerOpen(Gravity.RIGHT)) {
+                    drawerLayout.openDrawer(Gravity.RIGHT);
+                }
+                break;
+
+
         }
     }
 
@@ -45,16 +87,19 @@ public class ToolbarActivity extends AppCompatActivity implements View.OnClickLi
         toolbar = findViewById(R.id.toolbar);
         btnCredit = findViewById(R.id.btnCredit);
         btnMore = findViewById(R.id.btnMore);
-
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navView = findViewById(R.id.navigation);
+        btnDrawer = findViewById(R.id.btnDrawer);
     }
 
     private void clickEvents() {
         btnCredit.setOnClickListener(this);
         btnMore.setOnClickListener(this);
+        btnDrawer.setOnClickListener(this);
     }
 
     public void onPopupButtonClick(View button) {
-        Context myContext = new ContextThemeWrapper(ToolbarActivity.this,R.style.menuStyle);
+        Context myContext = new ContextThemeWrapper(ToolbarActivity.this, R.style.menuStyle);
         PopupMenu popup = new PopupMenu(myContext, button);
         popup.getMenuInflater().inflate(R.menu.popup_menu, popup.getMenu());
         popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
@@ -65,5 +110,25 @@ public class ToolbarActivity extends AppCompatActivity implements View.OnClickLi
         });
         popup.show();
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.nav_items, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+        switch (itemId) {
+            // Android home
+            case android.R.id.home:
+                drawerLayout.openDrawer(Gravity.RIGHT);
+                return true;
+        }
+        return true;
+    }
+
 
 }
