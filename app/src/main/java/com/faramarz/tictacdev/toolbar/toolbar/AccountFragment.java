69bbb3076be0e84d57dbf4a6865326caaf828498
FaramarzAf <faramarzafzali@gmail.com>
2019-10-24 13:10:51 +0330

    fragment bundle removed

--- app/src/main/java/com/faramarz/tictacdev/toolbar/toolbar/AccountFragment.java
+++ app/src/main/java/com/faramarz/tictacdev/toolbar/toolbar/AccountFragment.java
@@ -13,10 +13,8 @@ import android.widget.EditText;
 
 import com.faramarz.tictacdev.toolbar.R;
 
-public class AccountFragment extends Fragment implements View.OnClickListener {
+public class AccountFragment extends Fragment {
 
-    EditText edtCash;
-    Button btnPassCash;
 
     public AccountFragment() {
         // Required empty public constructor
@@ -25,26 +23,9 @@ public class AccountFragment extends Fragment implements View.OnClickListener {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
         View view = inflater.inflate(R.layout.fragment_account, container, false);
-        edtCash = view.findViewById(R.id.edtCash);
-        btnPassCash = view.findViewById(R.id.btnPassCash);
-        btnPassCash.setOnClickListener(this);
         return view;
 
     }
 
-
-    @Override
-    public void onClick(View v) {
-        CreditFragment fragment = new CreditFragment();
-        Bundle bundle = new Bundle();
-        String myCash = edtCash.getText().toString();
-        bundle.putString("myCash", myCash);
-        fragment.setArguments(bundle);
-        if (getFragmentManager() != null) {
-            getFragmentManager().beginTransaction().replace(R.id.frame, fragment).commit();
-        }
-
-    }
 }
--- app/src/main/java/com/faramarz/tictacdev/toolbar/toolbar/CreditFragment.java
+++ app/src/main/java/com/faramarz/tictacdev/toolbar/toolbar/CreditFragment.java
@@ -15,35 +15,20 @@ import com.faramarz.tictacdev.toolbar.R;
 
 
 public class CreditFragment extends Fragment {
-    TextView txtCash;
+
 
     public CreditFragment() {
         // Required empty public constructor
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
         View view = inflater.inflate(R.layout.fragment_credit, container, false);
-        txtCash = view.findViewById(R.id.txtCash);
-        receiveData();
         return view;
     }
 
-
-    private void receiveData() {
-        Bundle bundle = this.getArguments();
-        String myInt ;
-        if (bundle != null) {
-            myInt = bundle.getString("myCash", "");
-            txtCash.setText(myInt);
-        }
-    }
 
 
 }
